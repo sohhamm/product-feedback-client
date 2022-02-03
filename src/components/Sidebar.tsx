8663a9c4b18e5b9de513a9c71bad5655a19a8031
Soham Sarkar <soham.ss1@gmail.com>
2022-02-03 08:00:13 +0530

    feat: home sidebar (roadmap ui)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import {Box, Flex, Heading, Text} from '@chakra-ui/react'
-import {allTags} from '../constants/contants'
+import {allTags} from '../constants/constants'
 import Tag from './ui/Tag'
+import Link from 'next/link'
 
 export default function Sidebar() {
   return (
@@ -35,7 +36,74 @@ export default function Sidebar() {
           <Tag key={tag}>{tag}</Tag>
         ))}
       </Flex>
-      <Box w="255px" h="137px" borderRadius="10px" px="24px"></Box>
+      <Box w="255px" borderRadius="10px" p={['24px']} bg="white">
+        <Flex justify="space-between" mb="24px">
+          <Heading fontSize={['20px']} color="navyBlue2" fontWeight={'bold'}>
+            Roadmap
+          </Heading>
+          <Link href="/roadmap" passHref>
+            <Text
+              color="darkBlue"
+              textDecor={'underline'}
+              fontWeight={'semibold'}
+              cursor={'pointer'}
+            >
+              View
+            </Text>
+          </Link>
+        </Flex>
+
+        <Flex justify="space-between" mb="8px" fontSize={['16px']}>
+          <Flex align={'center'}>
+            <Box
+              as="span"
+              borderRadius={'60%'}
+              w="8px"
+              h="8px"
+              bg="#F49F85"
+              mr="16px"
+            />
+            <Text color="darkGray.400">Planned</Text>
+          </Flex>
+          <Text color="darkGray.400" fontWeight={'bold'}>
+            2
+          </Text>
+        </Flex>
+
+        <Flex justify="space-between" mb="8px" fontSize={['16px']}>
+          <Flex align={'center'}>
+            <Box
+              as="span"
+              borderRadius={'60%'}
+              w="8px"
+              h="8px"
+              bg="#AD1FEA"
+              mr="16px"
+            />
+            <Text color="darkGray.400">In-progress</Text>
+          </Flex>
+          <Text color="darkGray.400" fontWeight={'bold'}>
+            3
+          </Text>
+        </Flex>
+
+        <Flex justify="space-between" mb="8px" fontSize={['16px']}>
+          <Flex align={'center'}>
+            <Box
+              as="span"
+              borderRadius={'60%'}
+              w="8px"
+              h="8px"
+              bg="#62BCFA"
+              mr="16px"
+            />
+            <Text color="darkGray.400">Live</Text>
+          </Flex>
+          <Text color="darkGray.400" fontWeight={'bold'}>
+            1
+          </Text>
+        </Flex>
+      </Box>
     </Flex>
   )
 }
--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,22 +1,24 @@
-import {Box} from '@chakra-ui/react'
-import React from 'react'
 import {useUIStore} from '../../store/ui'
+import {Box} from '@chakra-ui/react'
 
 export default function Tag({children}: {children: string}) {
   const activeTag = useUIStore(state => state.activeTag)
+  const setActiveTag = useUIStore(state => state.setActiveTag)
+  const isSelected = activeTag === children
   return (
     <Box
       px="16px"
       py="5px"
-      color={activeTag === children ? 'white' : 'darkblue'}
-      bg={activeTag === children ? 'purple.400' : 'lightGray.400'}
+      color={isSelected ? 'white' : 'darkblue'}
+      bg={isSelected ? 'purple.400' : 'lightGray.400'}
       fontWeight={'semibold'}
       fontSize={'13px'}
       borderRadius={'12px'}
       _hover={{
-        bg: '#CFD7FF',
+        bg: isSelected ? '' : '#CFD7FF',
         cursor: 'pointer',
       }}
+      onClick={() => setActiveTag(children)}
     >
       {children}
     </Box>
