346dab351123f152124a55946347022f881070a2
Soham Sarkar <soham.ss1@gmail.com>
2022-02-27 13:12:32 +0530

    mobile drawer

--- src/components/mobile/MobileHeader.tsx
+++ src/components/mobile/MobileHeader.tsx
@@ -1,8 +1,27 @@
 import * as React from 'react'
-import {Box, Flex, Image, Heading, Text} from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Image,
+  Heading,
+  Text,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+} from '@chakra-ui/react'
 import HamburgerIcon from '/public/assets/shared/mobile/icon-hamburger.svg'
+import CloseIcon from '/public/assets/shared/mobile/icon-close.svg'
+import {allTags} from '@/constants/constants'
+import Tag from '../ui/Tag'
+import Link from 'next/link'
+import Status from '../ui/Status'
 
 export default function MobileHeader() {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const btnRef = React.useRef(null)
+
   return (
     <Flex
       as="header"
@@ -26,7 +45,90 @@ export default function MobileHeader() {
           Feedback Board
         </Text>
       </Box>
-      <Image src={HamburgerIcon.src} alt="Menu" h="17px" w="20px" />
+      <Image
+        src={isOpen ? CloseIcon.src : HamburgerIcon.src}
+        alt="Menu"
+        h="17px"
+        w="20px"
+        ref={btnRef}
+        onClick={onOpen}
+      />
+
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="xs"
+      >
+        <DrawerOverlay h="91.5vh" mt="72px" />
+        <DrawerContent h="91.5vh" mt="auto" bg="#F7F8FD">
+          <DrawerBody
+            display={'flex'}
+            flexDir="column"
+            alignItems="center"
+            rowGap={'24px'}
+            mt="24px"
+          >
+            <Flex
+              alignItems={'center'}
+              flexWrap={'wrap'}
+              gap="14px"
+              columnGap={['8px', '8px', '14px']}
+              w="255px"
+              borderRadius="10px"
+              p="24px"
+              bg="white"
+            >
+              {allTags.map(tag => (
+                <Tag key={tag}>{tag}</Tag>
+              ))}
+            </Flex>
+            <Box w="255px" borderRadius="10px" p={['24px']} bg="white">
+              <Flex justify="space-between" mb="24px">
+                <Heading
+                  fontSize={['18px', '18px', '20px']}
+                  color="navyBlue2"
+                  fontWeight={'bold'}
+                >
+                  Roadmap
+                </Heading>
+                <Link href="/roadmap" passHref>
+                  <Text
+                    color="darkBlue"
+                    textDecor={'underline'}
+                    fontWeight={'semibold'}
+                    cursor={'pointer'}
+                  >
+                    View
+                  </Text>
+                </Link>
+              </Flex>
+
+              <Flex justify="space-between" mb="8px" fontSize={['16px']}>
+                <Status color="#F49F85" text="Planned" isHomePage />
+                <Text color="darkGray.400" fontWeight={'bold'}>
+                  2
+                </Text>
+              </Flex>
+
+              <Flex justify="space-between" mb="8px" fontSize={['16px']}>
+                <Status color="#AD1FEA" text="In-progress" isHomePage />
+                <Text color="darkGray.400" fontWeight={'bold'}>
+                  3
+                </Text>
+              </Flex>
+
+              <Flex justify="space-between" mb="8px" fontSize={['16px']}>
+                <Status color="#62BCFA" text="Live" isHomePage />
+                <Text color="darkGray.400" fontWeight={'bold'}>
+                  1
+                </Text>
+              </Flex>
+            </Box>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Flex>
   )
 }
